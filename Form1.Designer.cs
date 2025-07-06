7c1463a8c36a051c509c43d810898beb4eadc01b
Elif <198382528+Elif999@users.noreply.github.com>
2025-07-06 15:20:39 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblDurum = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTranskript = new System.Windows.Forms.RichTextBox();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.btnCalistir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "YT url";
+            // 
+            // lblDurum
+            // 
+            this.lblDurum.AutoSize = true;
+            this.lblDurum.Location = new System.Drawing.Point(97, 171);
+            this.lblDurum.Name = "lblDurum";
+            this.lblDurum.Size = new System.Drawing.Size(16, 13);
+            this.lblDurum.TabIndex = 11;
+            this.lblDurum.Text = "...";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 173);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "durum:";
+            // 
+            // txtTranskript
+            // 
+            this.txtTranskript.Location = new System.Drawing.Point(58, 189);
+            this.txtTranskript.Name = "txtTranskript";
+            this.txtTranskript.Size = new System.Drawing.Size(328, 143);
+            this.txtTranskript.TabIndex = 9;
+            this.txtTranskript.Text = "";
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(100, 119);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(646, 20);
+            this.txtURL.TabIndex = 8;
+            // 
+            // btnCalistir
+            // 
+            this.btnCalistir.Location = new System.Drawing.Point(100, 145);
+            this.btnCalistir.Name = "btnCalistir";
+            this.btnCalistir.Size = new System.Drawing.Size(75, 23);
+            this.btnCalistir.TabIndex = 7;
+            this.btnCalistir.Text = "çalıştır";
+            this.btnCalistir.UseVisualStyleBackColor = true;
+            this.btnCalistir.Click += new System.EventHandler(this.btnCalistir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblDurum);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTranskript);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.btnCalistir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblDurum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox txtTranskript;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button btnCalistir;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,190 @@
+﻿using System.Text.Json;
+using NAudio.Wave;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using Vosk;
+using System.Speech.Synthesis;
+
+
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        string tempKlasor = Application.StartupPath + @"\Temp";
+        string videoDosyasi = "";
+        string sesDosyasi = "";
+
+        // EXE dosyalarının tam yolu, Application.StartupPath ile karışıklık olmasın diye tam yazdım:
+        string ytDlpYolu = @"C:\Users\user\source\repos\WindowsFormsApp3\WindowsFormsApp3\voskTest\yt-dlp.exe";
+        string ffmpegYolu = @"C:\Users\user\source\repos\WindowsFormsApp3\WindowsFormsApp3\voskTest\ffmpeg.exe";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            if (!Directory.Exists(tempKlasor))
+                Directory.CreateDirectory(tempKlasor);
+
+            lblDurum.Text = "Hazır";
+        }
+
+        private void btnCalistir_Click(object sender, EventArgs e)
+        {
+            string url = txtURL.Text;
+            if (string.IsNullOrWhiteSpace(url))
+            {
+                MessageBox.Show("Lütfen geçerli bir YouTube URL'si girin.");
+                return;
+            }
+
+            videoDosyasi = Path.Combine(tempKlasor, "video.mp4");
+            sesDosyasi = Path.Combine(tempKlasor, "orijinal_ses.wav");
+
+            try
+            {
+                lblDurum.Text = "Video indiriliyor...";
+                Application.DoEvents();
+
+                ProcessStartInfo psiYtdlp = new ProcessStartInfo
+                {
+                    FileName = ytDlpYolu,
+                    Arguments = $"-f bestvideo+bestaudio --merge-output-format mp4 -o \"{videoDosyasi}\" {url}",
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+                var procYtdlp = Process.Start(psiYtdlp);
+                procYtdlp.WaitForExit();
+
+                lblDurum.Text = "Video indirildi, ses ayıklanıyor...";
+                Application.DoEvents();
+
+                // Ses ayıklarken 16kHz mono olması çok önemli:
+                ProcessStartInfo psiFfmpegSes = new ProcessStartInfo
+                {
+                    FileName = ffmpegYolu,
+                    Arguments = $"-i \"{videoDosyasi}\" -vn -acodec pcm_s16le -ar 16000 -ac 1 \"{sesDosyasi}\"",
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+                var procFfmpegSes = Process.Start(psiFfmpegSes);
+                procFfmpegSes.WaitForExit();
+
+                lblDurum.Text = "Ses ayıklandı, yazıya dökülüyor...";
+                Application.DoEvents();
+
+                // Model yolu tam yazıldı:
+                string modelYolu = @"C:\Users\user\source\repos\WindowsFormsApp3\WindowsFormsApp3\voskTest\Model\vosk-model-small-tr-0.3\vosk-model-small-tr-0.3";
+
+                string rawResult = VoskSpeechToText(sesDosyasi, modelYolu);
+
+                // JSON içerisinden sadece text kısmını alıyoruz:
+                string metin = ExtractTextFromVoskResult(rawResult);
+
+                txtTranskript.Text = metin;
+
+                lblDurum.Text = "Metin işleniyor, seslendiriliyor...";
+                Application.DoEvents();
+
+                string cevrilmisMetin = metin; // İstersen burada Google Translate kullanabilirsin
+
+                string ttsDosyasi = Path.Combine(tempKlasor, "tts_ses.wav");
+                MetniSeslendir(cevrilmisMetin, ttsDosyasi);
+
+                lblDurum.Text = "Seslendirildi, video ile birleştiriliyor...";
+                Application.DoEvents();
+
+                string ciktiVideo = Path.Combine(tempKlasor, "sonuc_video.mp4");
+                VideoVeSesBirlestir(videoDosyasi, ttsDosyasi, ciktiVideo);
+
+                lblDurum.Text = "İşlem tamamlandı! Çıktı: " + ciktiVideo;
+                MessageBox.Show("İşlem tamamlandı! Dosya: " + ciktiVideo);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message);
+                lblDurum.Text = "Hata oluştu!";
+            }
+        }
+
+        public string VoskSpeechToText(string sesDosyasi, string modelYolu)
+        {
+            try
+            {
+                Vosk.Vosk.SetLogLevel(0);
+                StringBuilder sb = new StringBuilder();
+
+                using (var model = new Model(modelYolu))
+                using (var waveReader = new WaveFileReader(sesDosyasi))
+                using (var rec = new VoskRecognizer(model, 16000))
+                {
+                    byte[] buffer = new byte[2048];
+                    int bytesRead;
+
+                    while ((bytesRead = waveReader.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        if (rec.AcceptWaveform(buffer, bytesRead))
+                        {
+                            var result = rec.Result();
+                            sb.Append(result);
+                        }
+                    }
+                    sb.Append(rec.FinalResult());
+                }
+
+                return sb.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Vosk HATASI: " + ex.ToString(), "HATA!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return "";
+            }
+        }
+
+        // Vosk'tan gelen JSON formatındaki sonucu sade metne çeviriyoruz
+        public string ExtractTextFromVoskResult(string voskJson)
+        {
+            try
+            {
+                var jsonDoc = JsonDocument.Parse(voskJson);
+                if (jsonDoc.RootElement.TryGetProperty("text", out var textProp))
+                {
+                    return textProp.GetString();
+                }
+            }
+            catch
+            {
+                // JSON parse edilemezse orijinal string döner
+            }
+            return voskJson;
+        }
+
+        public void MetniSeslendir(string metin, string ciktiDosyasi)
+        {
+            using (SpeechSynthesizer synth = new SpeechSynthesizer())
+            {
+                synth.SelectVoiceByHints(VoiceGender.NotSet, VoiceAge.NotSet, 0, new System.Globalization.CultureInfo("tr-TR"));
+                synth.SetOutputToWaveFile(ciktiDosyasi);
+                synth.Speak(metin);
+            }
+        }
+
+        public void VideoVeSesBirlestir(string videoDosyasi, string sesDosyasi, string ciktiDosyasi)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = ffmpegYolu,
+                Arguments = $"-i \"{videoDosyasi}\" -i \"{sesDosyasi}\" -c:v copy -map 0:v:0 -map 1:a:0 -shortest \"{ciktiDosyasi}\"",
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+            var proc = Process.Start(psi);
+            proc.WaitForExit();
+        }
+    }
+}
+
+
